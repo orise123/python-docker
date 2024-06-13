b27396728bfee185780b9f23113e74510aa1d009
oriser <oriserban@gmail.com>
2024-06-13 18:26:03 +0300

    a

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import os
 app = Flask(__name__)
+
+if os.path.exists('counter.txt'):
+    with open('counter.txt', 'r') as file:
+        counter = int(file.read())
+else:
+    counter = 0
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    global counter
+    counter += 1
+    with open('counter.txt', 'w') as file:
+        file.write(str(counter))
+    return f"Hello! This page has been visited {counter} times."
